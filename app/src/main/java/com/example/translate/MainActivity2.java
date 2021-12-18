34f90297a2fae84b7a6b2c6b002d3521eecb5ce8
zeynab-krbsn <zeynab.krbsn@gmail.com>
2021-12-18 21:23:02 +0330

    show result in "TextView" when searched in "EditText".

--- app/src/main/java/com/example/translate/MainActivity2.java
+++ app/src/main/java/com/example/translate/MainActivity2.java
@@ -1,40 +1,32 @@
 package com.example.translate;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.app.DownloadManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.lang.ref.ReferenceQueue;
-import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
 
     private EditText editTextSearch;
-    private RecyclerView recyclerView;
-    private ArrayList<WordsModel> wordsModelArrayList;
-    private WordAdapter wordAdapter;
-    public String txtSearch;
+//    public String txtSearch;
     Context context;
+    private TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +35,8 @@ public class MainActivity2 extends AppCompatActivity {
 
 
         editTextSearch=findViewById(R.id.editTextSearch);
-        wordsModelArrayList=new ArrayList<>();
-        wordAdapter = new WordAdapter(wordsModelArrayList,this);
-        recyclerView=findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(wordAdapter);
-        getWordData();
-        //txtSearch= editTextSearch.getText().toString();
+        textViewResult=findViewById(R.id.textViewResult);
+//        getWordData();
         editTextSearch.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -58,39 +45,25 @@ public class MainActivity2 extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                String txtSearch= editTextSearch.getText().toString();
+                Log.d("test",txtSearch);
+                if (txtSearch.isEmpty()){
+                    textViewResult.setText("");
+                    return;
+                }
+                getWordData(txtSearch);
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                filterWords(s.toString());
+
             }
         });
 
     }
 
+    private void getWordData(String txtSearch) {
 
-
-    private void filterWords(String word) {
-        ArrayList<WordsModel> filterList = new ArrayList<>();
-        for (WordsModel item : wordsModelArrayList){
-            if (item.getWord().toLowerCase().contains(word.toLowerCase())){
-                filterList.add(item);
-            }
-        }
-        if(filterList.isEmpty()){
-            Toast.makeText(getApplicationContext(),"fail is Empty",Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            wordAdapter.filterList(filterList);
-        }
-    }
-
-
-    private void getWordData() {
-        txtSearch= editTextSearch.getText().toString();
-        //txtSearch="hi";
         String url = "https://one-api.ir/translate/?token=153856:61bba509e8ce37.54039805&action=google&lang=fa&q="+txtSearch;
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -98,14 +71,9 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    String dataArray=response.getString("result");
-                    editTextSearch.setText(dataArray);
-//                    for (int i = 0; i<dataArray.length(); i++) {
-//                        JSONObject dataObject = dataArray.getJSONObject(i);
-////                        String q=dataObject.getString("result");
-//                        wordsModelArrayList.add(new WordsModel("q"));
-//                    }
-                    wordAdapter.notifyDataSetChanged();
+                    String result=response.getString("result");
+                    textViewResult.setText(result);
+
                 }
                 catch (JSONException e){
                     e.printStackTrace();
