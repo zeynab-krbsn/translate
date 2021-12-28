1a7aaa9a172ae37da9093ce1dfe15032c3dc760d
zeynab-krbsn <zeynab.krbsn@gmail.com>
2021-12-28 17:56:00 +0330

    Splash Screen is complete

--- app/src/main/java/com/example/translate/MainActivity.java
+++ app/src/main/java/com/example/translate/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
@@ -13,16 +14,25 @@ import android.widget.Toast;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
+    CountDownTimer countDownTimer;
+    boolean CounterActive = true;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        seekBar=findViewById(R.id.seekBar);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent);
+            }
+        },SPLASH_TIME_OUT);
     }
 }
